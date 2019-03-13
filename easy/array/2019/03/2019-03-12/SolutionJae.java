f9ac152478c2b7264020a793785f77989aa49522
Jae <yuziyong.jae@gmail.com>
2019-03-13 09:20:42 +0800

    add Jae's solution

---
+++ easy/array/2019/03/2019-03-12/SolutionJae.java
@@ -0,0 +1,19 @@
+public class SolutionJae{
+    public int removeElement(int[] nums, int val)
+    {
+        if (nums.length == 0)
+        {
+            return 0;
+        }
+
+        int i = 0;
+        for (int j = 0; j < nums.length; j++)
+        {
+            if (nums[j] != val)
+            {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
