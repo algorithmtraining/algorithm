30d208dbc255b3a1516a0436c56ef40e57f5706b
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-03-19 16:05:25 +0800

    Update Solution_PH.java

--- easy/array/2019/03/2019-03-19/Solution_PH.java
+++ easy/array/2019/03/2019-03-19/Solution_PH.java
@@ -8,12 +8,10 @@ class Solution {
          }
        List<Integer> arr1 = new ArrayList<>(); //第一行元素
         arr1.add(1);
-        
         result.add(arr1);
         for(int i = 2;i< numRows+1;i++)   //从第二行开始循环
         { 
             List<Integer> temp = result.get(i-2);  //临时数组，存储上一行元素
-          
             List<Integer> nowarr = new ArrayList<>();  //当前行元素
             for(int j =0;j < i ;j++)  //当前行元素的首个和最后一个均为1
             {
@@ -25,13 +23,9 @@ class Solution {
                 {
                    nowarr.add(temp.get(j-1)+temp.get(j));
                 }
-                
             }
-           
-            result.add(nowarr);
-                    
+            result.add(nowarr);         
         }
-        
         return result;
     }
 }
