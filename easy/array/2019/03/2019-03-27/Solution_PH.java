26748cbf4ed9f3a38703ff7647145b1c50d8c536
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-03-28 15:58:06 +0800

    Update Solution_PH.java

--- easy/array/2019/03/2019-03-27/Solution_PH.java
+++ easy/array/2019/03/2019-03-27/Solution_PH.java
@@ -15,3 +15,22 @@ class Solution {           //Runtime: 84 ms, faster than 21.46% of Java online s
         }
     }
 }
+//两次循环    
+class Solution {  // Runtime: 335 ms, faster than 6.42% of Java online submissions for Contains Duplicate.
+                  // Memory Usage: 43.1 MB, less than 25.08% of Java online submissions for Contains Duplicate.
+    public boolean containsDuplicate(int[] nums) {
+        
+        for(int i = 0; i<nums.length;i++)
+        {
+            for(int j =i+1;j<nums.length;j++)
+            {
+                 if(nums[i] == nums[j])
+              {
+                return true;
+              }
+                  
+            }         
+        }
+        return false;
+    }
+}
