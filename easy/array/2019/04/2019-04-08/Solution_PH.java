89892c3c492e0b5b49372bf70e9047176a514038
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-04-09 15:47:07 +0800

    Create Solution_PH.java

---
+++ easy/array/2019/04/2019-04-08/Solution_PH.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map <Integer,Integer> map1 = new HashMap<>();
+         Map <Integer,Integer> map2= new HashMap<>();
+      int[] arraylist = new int[nums2.length]; 
+       
+        for(int i =0;i<nums1.length;i++)
+        {
+            map1.put(nums1[i],i);
+        }
+        for(int i =0;i<nums2.length;i++)
+        {
+            map2.put(nums2[i],i);
+        }
+        int j =0;
+        int k = 0;
+         for(int i =0;i<nums1.length;i++)
+        {
+             
+          if( map2.containsKey(nums1[i]) && map1.get(nums1[i]) == i  )
+          {
+              arraylist[j++]=nums1[i];
+              k++;
+              
+          }
+        }
+         int[] result = new int[k]; 
+        for(int i =0;i<k;i++)
+        {
+            result[i] = arraylist[i];
+        }
+        return result;
+    }
+}
