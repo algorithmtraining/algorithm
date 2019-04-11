8aed6ccc7d3d27a0d83b4ab29a159c039209d189
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-04-11 09:43:10 +0800

    Create Solution_PH.java

---
+++ easy/array/2019/04/2019-04-10/Solution_PH.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int lastcount =0;
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            
+            if(nums[i]==1)
+            {
+                count++;
+            }
+            else
+            {  
+                 count=0;
+                continue;
+            }
+            lastcount= Math.max(count,lastcount);
+        }
+        return lastcount;
+    }
+}
