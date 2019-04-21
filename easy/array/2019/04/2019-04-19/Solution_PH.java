bccd9dcfc3a115266d48e947ed508f9819a0cc2f
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-04-21 13:47:09 +0800

    Update Solution_PH.java

--- easy/array/2019/04/2019-04-19/Solution_PH.java
+++ easy/array/2019/04/2019-04-19/Solution_PH.java
@@ -17,3 +17,24 @@ class Solution { //Runtime: 1347 ms, faster than 6.59% of Java online submi
         return max;
     }    
 }
+//参考于姓大佬的写法
+public class Solution{
+    public double findMaxAverage(int[] nums, int k) {
+      int count =0;
+      int sum = 0;
+      double max = 0;
+        for(int i =0;i<nums.length;i++)
+        {
+            if(count<k)
+            {
+                sum += nums[i];
+                count ++ ;
+                max = sum;
+                continue;
+            }
+            sum = sum +nums[i] -nums[i-k];
+            max = Math.max(max,sum);
+        }
+        return max/k;
+    }
+}
