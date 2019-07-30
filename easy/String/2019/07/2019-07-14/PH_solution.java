fd407e499586345a4a1a3b7f9fd10fd0516e62dd
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-07-30 17:32:50 +0800

    Create PH_solution.java

---
+++ easy/String/2019/07/2019-07-14/PH_solution.java
@@ -0,0 +1,19 @@
+public class Solution { 
+     //本题意为相加两个由数字组成的字符串和
+//Runtime: 2 ms, faster than 95.24% of Java online submissions for Add Strings.
+  //Memory Usage: 36.2 MB, less than 99.61% of Java online submissions for Add Strings.
+              //用while循环超出内存大小，换用for循环测试通过         
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0; i--, j--){
+            int x = i < 0 ? 0 : num1.charAt(i) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        if(carry != 0)
+            sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
