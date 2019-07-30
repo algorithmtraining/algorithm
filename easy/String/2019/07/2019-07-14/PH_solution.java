b9f532d73dc7c24a890b4b6bc9c2b7703fac1679
huanpeng222 <48380482+huanpeng222@users.noreply.github.com>
2019-07-30 17:42:54 +0800

    Update PH_solution.java

--- easy/String/2019/07/2019-07-14/PH_solution.java
+++ easy/String/2019/07/2019-07-14/PH_solution.java
@@ -4,16 +4,16 @@ public class Solution {
   //Memory Usage: 36.2 MB, less than 99.61% of Java online submissions for Add Strings.
               //用while循环超出内存大小，换用for循环测试通过         
     public String addStrings(String num1, String num2) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         int carry = 0;
         for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0; i--, j--){
             int x = i < 0 ? 0 : num1.charAt(i) - '0';
             int y = j < 0 ? 0 : num2.charAt(j) - '0';
-            sb.append((x + y + carry) % 10);
+            result.append((x + y + carry) % 10);
             carry = (x + y + carry) / 10;
         }
         if(carry != 0)
-            sb.append(carry);
+            result.append(carry);
         return sb.reverse().toString();
     }
 }
